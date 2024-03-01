cb23b9d0a1fe44675cee20eb819db0bc731ab57f
vas24757 <vas24757@uvg.edu.gt>
2024-02-29 20:08:36 -0600

    File update

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,17 +1,10 @@
 import React from "react";
 import { useRoutes } from "react-router-dom";
-import Home from "pages/Home";
-import NotFound from "pages/NotFound";
 import Landingpage from "pages/Landingpage";
 
 const ProjectRoutes = () => {
   let element = useRoutes([
-    { path: "/", element: <Home /> },
-    { path: "*", element: <NotFound /> },
-    {
-      path: "landingpage",
-      element: <Landingpage />,
-    },
+    { path: "/", element: <Landingpage /> },
   ]);
 
   return element;
